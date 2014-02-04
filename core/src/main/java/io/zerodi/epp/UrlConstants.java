d235d444ab3115ae897be828b245160b3f44863d
zeroDivisible <zerodivisible@gmail.com>
2014-02-04 12:56:49 +0000

    - introduced basic resource which is giving details of an epp server

---
+++ core/src/main/java/io/zerodi/epp/UrlConstants.java
@@ -0,0 +1,9 @@
+package io.zerodi.epp;
+
+/**
+ * @author zerodi
+ */
+public class UrlConstants {
+
+    public static final String API_URL_PREFIX = "/api/v1";
+}
---
+++ core/src/main/java/io/zerodi/epp/resources/EppServerConfigurationResource.java
@@ -0,0 +1,37 @@
+package io.zerodi.epp.resources;
+
+import com.yammer.metrics.annotation.Timed;
+import io.zerodi.epp.UrlConstants;
+import io.zerodi.epp.core.EppServerDetails;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author zerodi
+ */
+@Path(UrlConstants.API_URL_PREFIX + "/epp-server-configuration")
+@Produces(MediaType.APPLICATION_JSON)
+public class EppServerConfigurationResource {
+
+    private final String serverAddress;
+    private final int serverPort;
+
+    private EppServerConfigurationResource(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+    }
+
+    public static EppServerConfigurationResource getInstance(String serverAddress, int serverPort) {
+        return new EppServerConfigurationResource(serverAddress, serverPort);
+    }
+
+
+    @GET
+    @Timed
+    public EppServerDetails getDetails() {
+        return EppServerDetails.getInstance(serverAddress, serverPort);
+    }
+}
