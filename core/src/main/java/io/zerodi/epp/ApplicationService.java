52b207f00b3420f68a8ff952abf744e81235970e
zeroDivisible <zerodivisible@gmail.com>
2014-02-05 09:57:56 +0000

    - introduced basic configuration for servier definitions - added health check, resource, service and representation for server definitions

---
+++ core/src/main/java/io/zerodi/epp/ApplicationService.java
@@ -0,0 +1,32 @@
+package io.zerodi.epp;
+
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+import io.zerodi.epp.healtchecks.ServerDefinitionHealthCheck;
+import io.zerodi.epp.resources.ServerDetailsResource;
+
+/**
+ * @author zerodi
+ */
+public class ApplicationService extends Service<GeneralServiceConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new ApplicationService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<GeneralServiceConfiguration> bootstrap) {
+        bootstrap.setName("server-wizard");
+
+        bootstrap.getObjectMapperFactory().enable(SerializationFeature.WRAP_ROOT_VALUE);
+    }
+
+    @Override
+    public void run(GeneralServiceConfiguration configuration, Environment environment) throws Exception {
+        environment.addResource(ServerDetailsResource.getInstance(configuration.getServers()));
+        environment.addHealthCheck(ServerDefinitionHealthCheck.getInstance(configuration.getServers()));
+    }
+}
