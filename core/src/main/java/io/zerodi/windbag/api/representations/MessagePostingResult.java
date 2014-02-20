9d475facfb0d0363c1a8354a5d8bad0e6d5abe13
zeroDivisible <zerodivisible@gmail.com>
2014-02-20 00:22:14 +0000

    added extraction of message type

--- core/src/main/java/io/zerodi/windbag/api/representations/MessagePostingResult.java
+++ core/src/main/java/io/zerodi/windbag/api/representations/MessagePostingResult.java
@@ -3,6 +3,7 @@ package io.zerodi.windbag.api.representations;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import io.zerodi.windbag.app.protocol.Message;
+import io.zerodi.windbag.app.protocol.MessageType;
 
 /**
  * This is the result of posting a message to a connection.
@@ -15,8 +16,12 @@ public class MessagePostingResult {
 	@JsonProperty("raw_response")
 	private String rawResponse;
 
+	@JsonProperty("message_type")
+	private MessageType messageType;
+
 	private MessagePostingResult(Message message) {
 		this.rawResponse = message.getMessage();
+		this.messageType = message.getType();
 	}
 
 	public static MessagePostingResult getInstance(Message message) {
