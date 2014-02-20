58ad4699a6d5ce6cf244583f6bad24c761e1dd6a
zeroDivisible <zerodivisible@gmail.com>
2014-02-20 00:44:51 +0000

    whitespace diff

--- core/src/main/java/io/zerodi/windbag/app/protocol/epp/EppConnection.java
+++ core/src/main/java/io/zerodi/windbag/app/protocol/epp/EppConnection.java
@@ -22,125 +22,125 @@ import java.util.concurrent.TimeUnit;
  * @author zerodi
  */
 public class EppConnection implements Connection {
-    private static final Logger logger = LoggerFactory.getLogger(EppConnection.class);
+	private static final Logger logger = LoggerFactory.getLogger(EppConnection.class);
 
-    private final ServerDetail serverDetail;
-    private ProtocolBootstrap protocolBootstrap;
-    private Channel channel = null;
-    private MessageExchange messageExchange = MessageExchangeImpl.getInstance();
+	private final ServerDetail serverDetail;
+	private ProtocolBootstrap protocolBootstrap;
+	private Channel channel = null;
+	private MessageExchange messageExchange = MessageExchangeImpl.getInstance();
 
-    private EppConnection(ServerDetail serverDetail, ProtocolBootstrap protocolBootstrap) {
-        this.serverDetail = serverDetail;
-        this.protocolBootstrap = protocolBootstrap;
-    }
+	private EppConnection(ServerDetail serverDetail, ProtocolBootstrap protocolBootstrap) {
+		this.serverDetail = serverDetail;
+		this.protocolBootstrap = protocolBootstrap;
+	}
 
-    public static Connection getInstance(ServerDetail serverDetail, ProtocolBootstrap protocolBootstrap) {
-        return new EppConnection(serverDetail, protocolBootstrap);
-    }
+	public static Connection getInstance(ServerDetail serverDetail, ProtocolBootstrap protocolBootstrap) {
+		return new EppConnection(serverDetail, protocolBootstrap);
+	}
 
-    @Override
-    public ChannelFuture connect() {
-        if (!isConnected()) {
-            String serverAddress = serverDetail.getServerAddress();
-            int serverPort = serverDetail.getServerPort();
+	@Override
+	public ChannelFuture connect() {
+		if (!isConnected()) {
+			String serverAddress = serverDetail.getServerAddress();
+			int serverPort = serverDetail.getServerPort();
 
-            logger.debug("{}:{} - connecting", serverAddress, serverPort);
-            Bootstrap bootstrap = protocolBootstrap.getBootstrap();
+			logger.debug("{}:{} - connecting", serverAddress, serverPort);
+			Bootstrap bootstrap = protocolBootstrap.getBootstrap();
 
-            EventLoopGroup group = bootstrap.group();
-            if (group == null) {
-                EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
-                bootstrap.group(eventLoopGroup);
-            }
+			EventLoopGroup group = bootstrap.group();
+			if (group == null) {
+				EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+				bootstrap.group(eventLoopGroup);
+			}
 
-            ChannelFuture connectionFuture = bootstrap.connect(serverAddress, serverPort);
-            connectionFuture.addListener(new ChannelFutureListener() {
-                @Override
-                public void operationComplete(ChannelFuture future) throws Exception {
-                    channel = future.channel();
-                    future.channel().pipeline().addLast(ResponseReceiver.getInstance(getMessageExchange()));
-                }
-            });
+			ChannelFuture connectionFuture = bootstrap.connect(serverAddress, serverPort);
+			connectionFuture.addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					channel = future.channel();
+					future.channel().pipeline().addLast(ResponseReceiver.getInstance(getMessageExchange()));
+				}
+			});
 
-            try {
-                // EPP Server will be greeting us with a greeting message - let's report that one here
-                connectionFuture.await(10, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                logger.error("while connecting to server", e);
-                throw new RuntimeException(e); // TODO created specialized exception type, not the generic RuntimeException.
-            }
-            return connectionFuture;
-        } else {
-            // TODO find a way to return completed future.
-            return null;
-        }
-    }
+			try {
+				// EPP Server will be greeting us with a greeting message - let's report that one here
+				connectionFuture.await(10, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				logger.error("while connecting to server", e);
+				throw new RuntimeException(e); // TODO created specialized exception type, not the generic RuntimeException.
+			}
+			return connectionFuture;
+		} else {
+			// TODO find a way to return completed future.
+			return null;
+		}
+	}
 
-    @Override
-    public boolean isConnected() {
-        if (channel == null) {
-            return false;
-        }
+	@Override
+	public boolean isConnected() {
+		if (channel == null) {
+			return false;
+		}
 
-        return channel.isActive();
-    }
+		return channel.isActive();
+	}
 
-    @Override
-    public ChannelFuture disconnect() {
-        if (channel != null) {
-            ChannelFuture channelFuture;
-            try {
-                channelFuture = channel.close().sync();
-                channel = null;
-            } catch (InterruptedException e) {
-                logger.error("while disconnecting from remote server", e);
-                throw new RuntimeException(e); // TODO handle better than this.
-            }
-            return channelFuture;
-        } else {
-            return null;
-        }
-    }
+	@Override
+	public ChannelFuture disconnect() {
+		if (channel != null) {
+			ChannelFuture channelFuture;
+			try {
+				channelFuture = channel.close().sync();
+				channel = null;
+			} catch (InterruptedException e) {
+				logger.error("while disconnecting from remote server", e);
+				throw new RuntimeException(e); // TODO handle better than this.
+			}
+			return channelFuture;
+		} else {
+			return null;
+		}
+	}
 
-    @Override
-    public ChannelFuture reconnect() {
-        disconnect();
-        return connect();
-    }
+	@Override
+	public ChannelFuture reconnect() {
+		disconnect();
+		return connect();
+	}
 
-    @Override
-    public Message sendMessage(Message message) {
-        Preconditions.checkNotNull(message, "message cannot be null!");
-        Preconditions.checkArgument(isConnected(), "connection needs to be open and active to send the messages!");
+	@Override
+	public Message sendMessage(Message message) {
+		Preconditions.checkNotNull(message, "message cannot be null!");
+		Preconditions.checkArgument(isConnected(), "connection needs to be open and active to send the messages!");
 
-        getMessageExchange().postMessage(message);
+		getMessageExchange().postMessage(message);
 
-        ResponseReceiver responseReceiver = (ResponseReceiver) ResponseReceiver.getInstance(getMessageExchange());
-        channel.pipeline().addLast("response-receiver", responseReceiver);
-        channel.writeAndFlush(message.asByteBuf());
+		ResponseReceiver responseReceiver = (ResponseReceiver) ResponseReceiver.getInstance(getMessageExchange());
+		channel.pipeline().addLast("response-receiver", responseReceiver);
+		channel.writeAndFlush(message.asByteBuf());
 
-        while (!responseReceiver.ifFinished()) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        return getMessageExchange().getLastMessage();
-    }
+		while (!responseReceiver.ifFinished()) {
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return getMessageExchange().getLastMessage();
+	}
 
-    @Override
-    public ProtocolBootstrap getProtocolBootstrap() {
-        return protocolBootstrap;
-    }
+	@Override
+	public ProtocolBootstrap getProtocolBootstrap() {
+		return protocolBootstrap;
+	}
 
-    @Override
-    public ServerDetail getServerDetail() {
-        return serverDetail;
-    }
+	@Override
+	public ServerDetail getServerDetail() {
+		return serverDetail;
+	}
 
-    @Override
-    public MessageExchange getMessageExchange() {
-        return messageExchange;
-    }
+	@Override
+	public MessageExchange getMessageExchange() {
+		return messageExchange;
+	}
 }
