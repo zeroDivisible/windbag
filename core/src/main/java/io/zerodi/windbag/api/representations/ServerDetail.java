59cbf85674b035cf8b0e1fd8280cc285132e766f
zeroDivisible <zerodivisible@gmail.com>
2014-02-26 23:25:33 +0000

    whitespace diff

--- core/src/main/java/io/zerodi/windbag/api/representations/ServerDetail.java
+++ core/src/main/java/io/zerodi/windbag/api/representations/ServerDetail.java
@@ -1,7 +1,6 @@
 package io.zerodi.windbag.api.representations;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
 import io.zerodi.windbag.core.Protocol;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -13,53 +12,53 @@ import javax.validation.constraints.Min;
  */
 public class ServerDetail {
 
-    @NotEmpty
-    @JsonProperty
-    private String name;
+	@NotEmpty
+	@JsonProperty
+	private String name;
 
-    @NotEmpty
-    @JsonProperty("server_address")
-    private String serverAddress;
+	@NotEmpty
+	@JsonProperty("server_address")
+	private String serverAddress;
 
-    @NotEmpty
-    @JsonProperty("server_port")
-    @Min(1)
-    @Max(65535)
-    private int serverPort;
+	@NotEmpty
+	@JsonProperty("server_port")
+	@Min(1)
+	@Max(65535)
+	private int serverPort;
 
-    @NotEmpty
-    @JsonProperty
-    private Protocol protocol;
+	@NotEmpty
+	@JsonProperty
+	private Protocol protocol;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getServerAddress() {
-        return serverAddress;
-    }
+	public String getServerAddress() {
+		return serverAddress;
+	}
 
-    public int getServerPort() {
-        return serverPort;
-    }
+	public int getServerPort() {
+		return serverPort;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setServerAddress(String serverAddress) {
-        this.serverAddress = serverAddress;
-    }
+	public void setServerAddress(String serverAddress) {
+		this.serverAddress = serverAddress;
+	}
 
-    public void setServerPort(int serverPort) {
-        this.serverPort = serverPort;
-    }
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
 
-    public Protocol getProtocol() {
-        return protocol;
-    }
+	public Protocol getProtocol() {
+		return protocol;
+	}
 
-    public void setProtocol(Protocol protocol) {
-        this.protocol = protocol;
-    }
+	public void setProtocol(Protocol protocol) {
+		this.protocol = protocol;
+	}
 }
