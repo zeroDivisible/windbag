9a3504d0a3c13db0864d4ce91a8a1a1617f96e80
zerodi <zerodivisible@gmail.com>
2014-03-02 22:03:10 +0000

    added basic settings for Ember to allow me debugging some basic operations

--- core/src/main/resources/assets/js/app/app.js
+++ core/src/main/resources/assets/js/app/app.js
@@ -1,3 +1,18 @@
+"use strict";
+
 window.Windbag = Ember.Application.create();
+
+Windbag.Store = DS.Store.extend({
+	revision: 13,
+	adapter: DS.RESTAdapter.create({
+		namespace: 'api/v1'
+	})
+});
+
+var attr = DS.attr;
+
+Windbag.Server = DS.Model.extend({
+	server_name: attr("string")
+});
 
 
--- core/src/main/resources/assets/js/app/routes.js
+++ core/src/main/resources/assets/js/app/routes.js
@@ -2,3 +2,11 @@ Windbag.Router.map(function () {
 	this.resource('index', { path: '/' });
 });
 
+
+Windbag.IndexRoute = Ember.Route.extend({
+	model: function () {
+		this.store.find('server').then(function (clients) {
+			console.log(clients);
+		});
+	}
+});
